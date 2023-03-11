d37834d617c8017efb9e3a559dd56dfd81abfa25
hasibul-miraz <hasibul.miraz@gmail.com>
2023-03-11 13:54:09 +0600

    added baseToken for grantToken and refreshToken

--- pgw/baseToken/apps.py
+++ pgw/baseToken/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CheckouturlConfig(AppConfig):
+class BasetokenConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "checkoutURL"
+    name = "baseToken"
