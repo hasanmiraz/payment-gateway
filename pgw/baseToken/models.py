268e80839121fea11256bbce4450c79ada3b490c
hasibul-miraz <hasibul.miraz@gmail.com>
2023-03-11 14:20:52 +0600

    grantToken model

--- pgw/baseToken/models.py
+++ pgw/baseToken/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class tokens(models.Model):
+    grant_token = models.TextField()
+    time_token_created = models.TimeField()
--- pgw/pgw/urls.py
+++ pgw/pgw/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("token/", include("baseToken.urls")),
     path("checkouturl/", include("checkoutURL.urls"))
 ]
