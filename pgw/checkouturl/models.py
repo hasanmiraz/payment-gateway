934d9e2ece6e943f8219b9db289ba6e3f5e8efeb
hasibul-miraz <hasibul.miraz@gmail.com>
2023-03-26 01:14:43 +0600

    test model added

--- pgw/checkouturl/models.py
+++ pgw/checkouturl/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class testWallet(models.Model):
+    number = models.PhoneNumberField(null=False, blank=False, unique=True)
+    pin = models.IntegerField(null=False, max_length=5)
+    otp = models.ImageField(null=False, max_length=5)
