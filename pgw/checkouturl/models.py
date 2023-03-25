96b8e962ecb3c9e92b10afa91877b8034565e3b3
hasibul-miraz <hasibul.miraz@gmail.com>
2023-03-26 01:28:03 +0600

    creating the test number class

--- pgw/checkouturl/models.py
+++ pgw/checkouturl/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class testWallet(models.Model):
-    number = models.PhoneNumberField(null=False, blank=False, unique=True)
+    number = models.IntegerField(null=False, blank=False, unique=True, max_length=11)
     pin = models.IntegerField(null=False, max_length=5)
-    otp = models.ImageField(null=False, max_length=5)
+    otp = models.IntegerField(null=False, max_length=5)
--- pgw/checkouturl/views.py
+++ pgw/checkouturl/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from . import models
 
 # Create your views here.
+
+def debugTool(**kwargs):
+    print("-"*30)
+    for key, value in kwargs.items():
+        print(f"task = {key}, taskResult = {value}\n")
+    print("-"*30)
+
+@api_view(["GET"])
+def test(request):
+    if request.method == "GET":
+        data = {
+            "name": "test"
+        }
+        return Response(data)
